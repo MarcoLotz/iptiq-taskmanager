b57512a9284eb6df3ab33ba1584b423a94dc8c2d
Marco Aurelio Lotz <lotzutfpr@gmail.com>
2021-03-04 22:25:59 +0100

    small log improvement

--- src/main/java/com/marcolotz/iptiq/core/model/AbstractProcess.java
+++ src/main/java/com/marcolotz/iptiq/core/model/AbstractProcess.java
@@ -7,7 +7,7 @@ import lombok.extern.log4j.Log4j2;
 public abstract class AbstractProcess {
 
   public void kill() {
-    log.info("Killing process:", this::toString);
+    log.info("Killing process: {}", this::toString);
   }
 
   public abstract UUID getPid();
