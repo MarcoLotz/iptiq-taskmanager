37443fdf5f66091c239f11473be00dfb3eeebc55
Marco Aurelio Lotz <lotzutfpr@gmail.com>
2021-04-02 19:46:36 +0200

    implement kill process based on pid

--- src/main/java/com/marcolotz/iptiq/adapters/inbound/rest/TaskManagerController.java
+++ src/main/java/com/marcolotz/iptiq/adapters/inbound/rest/TaskManagerController.java
@@ -1,5 +1,7 @@
 package com.marcolotz.iptiq.adapters.inbound.rest;
 
+import com.marcolotz.iptiq.core.SortingMethod;
+import com.marcolotz.iptiq.core.exceptions.ProcessNotFoundException;
 import com.marcolotz.iptiq.core.model.Process;
 import com.marcolotz.iptiq.ports.TaskManager;
 import com.marcolotz.taskmanager.openapi.handler.V1Api;
@@ -8,6 +10,7 @@ import com.marcolotz.taskmanager.openapi.model.PriorityTypes;
 import com.marcolotz.taskmanager.openapi.model.RunningProcessDTO;
 import com.marcolotz.taskmanager.openapi.model.SortingMethodDTO;
 import lombok.SneakyThrows;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,10 +21,14 @@ import javax.validation.constraints.DecimalMax;
 import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.NotNull;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
+import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RestController
+@Log4j2
 public class TaskManagerController implements V1Api {
 
     @Autowired
@@ -31,26 +38,30 @@ public class TaskManagerController implements V1Api {
     protected DtoToDomainMapper mapper;
 
     @Override
-    @SneakyThrows // TODO: Controller advice
     public ResponseEntity<Void> v1ProcessesDelete(final @DecimalMin("0") @DecimalMax("50") @Valid List<String> pids,
                                                   final @Valid PriorityTypes priorityGroup,
                                                   final @Valid Boolean killAll) {
         if (killAll) {
             taskManager.killAll();
         } else {
-            final Runnable killAllPids = () -> pids.forEach(pid -> {
-                try {
+            final Set<String> pidsFromPriorityGroup = Optional.ofNullable(priorityGroup)
+                .map(mapper::fromPriorityType)
+                .map(type -> taskManager.listRunningProcess(SortingMethod.PRIORITY).stream().filter(p -> p.getPriority().equals(type))
+                    .map(Process::getPid)
+                    .map(UUID::toString)
+                    .collect(Collectors.toSet()))
+                .orElse(Set.of());
+
+            final Set<String> pidsToKill = Stream.concat(pids.stream(), pidsFromPriorityGroup.stream()).collect(Collectors.toSet());
+
+            try { // As an implementation decision, I think it makes sense to fail on the first error
+                for (final String pid : pidsToKill) {
                     taskManager.killProcess(pid);
-                } catch (final Exception e) {
-                    // TODO
                 }
-            });
-            final CompletableFuture<Void> pidFuture = CompletableFuture.runAsync(killAllPids);
-
-            final Runnable killAllFromGroup = () -> taskManager.killGroup(mapper.fromPriorityType(priorityGroup));
-            final CompletableFuture<Void> groupFuture = CompletableFuture.runAsync(killAllFromGroup);
-
-            CompletableFuture.allOf(pidFuture, groupFuture).join();
+            } catch (final ProcessNotFoundException e) {
+                log.error("Process couldn't not be found", e);
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
         }
         return new ResponseEntity<>(HttpStatus.OK);
     }
