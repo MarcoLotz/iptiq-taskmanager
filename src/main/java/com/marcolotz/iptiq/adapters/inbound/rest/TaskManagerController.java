9ec9c6460dd6f1f6d9fd456a487777b3e06a1a69
Marco Aurelio Lotz <lotzutfpr@gmail.com>
2021-04-06 03:21:49 +0200

    Add tests for async controller

--- src/main/java/com/marcolotz/iptiq/adapters/inbound/rest/TaskManagerController.java
+++ src/main/java/com/marcolotz/iptiq/adapters/inbound/rest/TaskManagerController.java
@@ -12,9 +12,9 @@ import com.marcolotz.taskmanager.openapi.model.RunningProcessDTO;
 import com.marcolotz.taskmanager.openapi.model.SortingMethodDTO;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.request.async.DeferredResult;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
@@ -23,7 +23,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
-import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -38,19 +38,18 @@ public class TaskManagerController implements V1Api {
     protected DtoToDomainMapper mapper;
 
     @Override
-    public DeferredResult<ResponseEntity<Void>> v1ProcessesDelete(final @Size(max = 50) @Valid List<String> pids,
-                                                                  final @Valid PriorityTypes priorityGroup,
-                                                                  final @Valid Boolean killAll) {
+    public CompletableFuture<ResponseEntity<Void>> v1ProcessesDelete(final @Size(max = 50) @Valid List<String> pids,
+                                                                     final @Valid PriorityTypes priorityGroup,
+                                                                     final @Valid Boolean killAll) {
         log.debug("Received request to kill process with the following arguments: pids: {}, prioritygroup: {}, isKillAll: {}",
-            () -> String.join(",", pids), priorityGroup::toString, killAll::toString);
+            () -> pids == null ? "null" : String.join(",", pids),
+            () -> priorityGroup == null ? "null" : priorityGroup.toString(),
+            () -> killAll == null ? "null" : killAll.toString());
 
-        final DeferredResult<ResponseEntity<Void>> output = new DeferredResult<>();
-        ForkJoinPool.commonPool().submit(() -> performDeletion(pids, priorityGroup, killAll, output));
-        return output;
+        return CompletableFuture.supplyAsync(() -> performDeletion(pids, priorityGroup, killAll));
     }
 
-    private void performDeletion(final List<String> pids, final PriorityTypes priorityGroup, final Boolean killAll,
-                                 final DeferredResult<ResponseEntity<Void>> output) {
+    private ResponseEntity<Void> performDeletion(final List<String> pids, final PriorityTypes priorityGroup, final Boolean killAll) {
         if (killAll) {
             taskManager.killAll();
         } else {
@@ -68,43 +67,40 @@ public class TaskManagerController implements V1Api {
                 for (final String pid : pidsToKill) {
                     taskManager.killProcess(pid);
                 }
-                output.setResult(ResponseEntity.ok().build());
             } catch (final ProcessNotFoundException e) {
                 log.error("Process couldn't not be found", e);
-                output.setErrorResult(e);
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
             }
         }
+        return ResponseEntity.ok().build();
     }
 
     @Override
     // TODO: Use pagination
-    public DeferredResult<ResponseEntity<List<RunningProcessDTO>>> v1ProcessesGet(final @NotNull @Valid SortingMethodDTO sortingMethod) {
-        log.debug("getting all processes using the following sorting method: {}", sortingMethod::toString);
+    public CompletableFuture<ResponseEntity<List<RunningProcessDTO>>> v1ProcessesGet(final @NotNull @Valid SortingMethodDTO sortingMethod) {
+        log.debug("Getting all processes using the following sorting method: {}", sortingMethod::toString);
 
-        final DeferredResult<ResponseEntity<List<RunningProcessDTO>>> output = new DeferredResult<>();
-        ForkJoinPool.commonPool().submit(() -> {
+        return CompletableFuture.supplyAsync(() -> {
             final List<Process> runningProcesses = taskManager.listRunningProcess(mapper.fromSortingMethodDto(sortingMethod));
             final List<RunningProcessDTO> runningProcessDTOS =
                 runningProcesses.stream().map(mapper::toRunningProcessDto).collect(Collectors.toList());
-            output.setResult(ResponseEntity.ok(runningProcessDTOS));
+            return ResponseEntity.ok(runningProcessDTOS);
         });
-        return output;
     }
 
     @Override
-    public DeferredResult<ResponseEntity<Void>> v1ProcessesPut(final @Valid AddedProcessDTO addedProcessDTO) {
-        log.debug("creating the following process: {}", addedProcessDTO::toString);
-        final DeferredResult<ResponseEntity<Void>> output = new DeferredResult<>();
+    // Note - In this case, I rather use exception handler inside the controller than on a global ControllerAdvice
+    public CompletableFuture<ResponseEntity<Void>> v1ProcessesPut(final @Valid AddedProcessDTO addedProcessDTO) {
+        log.debug("Creating the following process: {}", addedProcessDTO::toString);
 
-        ForkJoinPool.commonPool().submit(() -> {
+        return CompletableFuture.supplyAsync(() -> {
             try {
                 taskManager.addProcess(mapper.fromAddedProcessDTO(addedProcessDTO));
-                output.setResult(ResponseEntity.ok().build());
             } catch (final MaximumCapacityReachedException e) {
                 log.error("Could not add new process to task manager, maximum capacity reached");
-                output.setErrorResult(e);
+                return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).build();
             }
+            return ResponseEntity.ok().build();
         });
-        return output;
     }
 }
---
+++ src/test/java/com/marcolotz/iptiq/adapters/inbound/rest/TaskManagerControllerIT.java
@@ -0,0 +1,185 @@
+package com.marcolotz.iptiq.adapters.inbound.rest;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.marcolotz.iptiq.core.SortingMethod;
+import com.marcolotz.iptiq.core.exceptions.MaximumCapacityReachedException;
+import com.marcolotz.iptiq.core.exceptions.ProcessNotFoundException;
+import com.marcolotz.iptiq.ports.TaskManager;
+import com.marcolotz.taskmanager.openapi.model.AddedProcessDTO;
+import com.marcolotz.taskmanager.openapi.model.PriorityTypes;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mapstruct.factory.Mappers;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.asyncDispatch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = TaskManagerController.class)
+@DisplayName("When using the task manager REST api")
+@Import(DtoToDomainConfiguration.class)
+// Keep in mind that ComponentScan doesn't play well with WebMvcTest: https://tinyurl.com/27nn5yws
+class TaskManagerControllerIT {
+
+    @MockBean
+    private TaskManager taskManager;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private final ObjectMapper serializer = new ObjectMapper();
+
+    @BeforeEach
+    void setup()
+    {
+        reset(taskManager);
+    }
+
+    @Test
+    @DisplayName("then all running processes will be provided with the required sorting method")
+    @SneakyThrows
+    void whenValidGetRequest_thenListProcesses() {
+        mockMvc.perform(get("/v1/processes").param("sortingMethod", "CREATION_TIME")
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk());
+
+        verify(taskManager).listRunningProcess(SortingMethod.CREATION_TIME);
+    }
+
+    @Test
+    @DisplayName("then it will create processes if capacity allows")
+    @SneakyThrows
+    void whenValidPutRequest_thenCreateProcess() {
+        // Given
+        AddedProcessDTO addedProcessDTO = new AddedProcessDTO();
+        addedProcessDTO.setPriority(PriorityTypes.HIGH);
+
+        // Expect
+        mockMvc.perform(put("/v1/processes")
+            .content(asJsonString(addedProcessDTO))
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk());
+
+        verify(taskManager).addProcess(any());
+    }
+
+    @Test
+    @DisplayName("then it will return a 503 if the capacity is exceeded")
+    @SneakyThrows
+    // Async tests require a different testing format:
+    // https://stackoverflow.com/questions/46908906/spring-boot-returning-wrong-status-code-only-in-unit-test
+    void whenCapacityIsFull_thenDenyProcessCreation() {
+        // Given
+        AddedProcessDTO addedProcessDTO = new AddedProcessDTO();
+        addedProcessDTO.setPriority(PriorityTypes.HIGH);
+
+        doThrow(new MaximumCapacityReachedException("boom")).when(taskManager).addProcess(any());
+
+        final MvcResult result = mockMvc.perform(put("/v1/processes")
+            .content(asJsonString(addedProcessDTO))
+            .contentType(MediaType.APPLICATION_JSON))
+            .andReturn();
+
+        // Expect
+        mockMvc.perform(asyncDispatch(result))
+            .andExpect(status().isServiceUnavailable());
+
+        verify(taskManager).addProcess(any());
+    }
+
+    @Test
+    @DisplayName("Then it will kill all process if requested")
+    @SneakyThrows
+    void whenKillAllIsEnabled_ThenAllProcessAreKilled()
+    {
+        // Given
+        doNothing().when(taskManager).killAll();
+
+        final MvcResult result = mockMvc.perform(delete("/v1/processes").param("killAll", "true")
+            .contentType(MediaType.APPLICATION_JSON))
+            .andReturn();
+
+        // Expect
+        mockMvc.perform(asyncDispatch(result))
+            .andExpect(status().isOk());
+
+        verify(taskManager).killAll();
+    }
+
+    @Test
+    @DisplayName("Then it will kill only the pids in the list")
+    @SneakyThrows
+    void whenPidsAreListed_ThenProcessesWithThatPidsWillBeKilled()
+    {
+        // Given
+        final String pid1 = "1";
+        final String pid2 = "2";
+        doNothing().when(taskManager).killProcess(pid1);
+        doNothing().when(taskManager).killProcess(pid2);
+
+        final MvcResult result = mockMvc.perform(delete("/v1/processes").param("pids", pid1 + "," + pid2)
+            .contentType(MediaType.APPLICATION_JSON))
+            .andReturn();
+
+        // Expect
+        mockMvc.perform(asyncDispatch(result))
+            .andExpect(status().isOk());
+
+        verify(taskManager).killProcess(pid1);
+        verify(taskManager).killProcess(pid2);
+    }
+
+    @Test
+    @DisplayName("Then it will return a 404 if a pid is not found")
+    @SneakyThrows
+    void whenNotFoundPidIsListed_ThenReturnNotFoundStatus()
+    {
+        // Given
+        final String pid1 = "1";
+        final String pid2 = "2";
+        doNothing().when(taskManager).killProcess(pid1);
+        doThrow(new ProcessNotFoundException("not found")).when(taskManager).killProcess(pid2);
+
+        final MvcResult result = mockMvc.perform(delete("/v1/processes").param("pids", pid1 + "," + pid2)
+            .contentType(MediaType.APPLICATION_JSON))
+            .andReturn();
+
+        // Expect
+        mockMvc.perform(asyncDispatch(result))
+            .andExpect(status().isNotFound());
+
+        verify(taskManager).killProcess(pid1);
+        verify(taskManager).killProcess(pid2);
+    }
+
+    private String asJsonString(final Object obj) {
+        try {
+            return serializer.writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
